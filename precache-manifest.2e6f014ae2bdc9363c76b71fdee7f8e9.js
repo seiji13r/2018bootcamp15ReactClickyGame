720def416063932ac7b45a43d81eed5036884585
Seiji Osada <seiji13r@outlook.com>
2019-01-17 11:58:37 -0600

    Updates

---
+++ precache-manifest.2e6f014ae2bdc9363c76b71fdee7f8e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/2018bootcamp15reactclickygame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7d466e890665bad87fad",
+    "url": "/2018bootcamp15reactclickygame/static/js/runtime~main.7d466e89.js"
+  },
+  {
+    "revision": "72ce9166818ad3cc17c3",
+    "url": "/2018bootcamp15reactclickygame/static/js/main.72ce9166.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/2018bootcamp15reactclickygame/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "72ce9166818ad3cc17c3",
+    "url": "/2018bootcamp15reactclickygame/static/css/main.33446e1e.chunk.css"
+  },
+  {
+    "revision": "a30674024551e0dcf50852bebbb1d840",
+    "url": "/2018bootcamp15reactclickygame/index.html"
+  }
+];
